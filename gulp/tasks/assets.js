ece2141d05c9f12da153473919a829af6c5abb20
Michael Foley <michaeldfoley@gmail.com>
2018-06-07 10:53:55 -0400

    removed preserveComments from uglify task

--- gulp/tasks/assets.js
+++ gulp/tasks/assets.js
@@ -35,7 +35,7 @@ gulp.task('scripts', () =>
       showFiles: true
     }))
     .pipe(when(argv.prod, rename({suffix: '.min'})))
-    .pipe(when(argv.prod, when('*.js', uglify({preserveComments: 'some'}))))
+    .pipe(when(argv.prod, when('*.js', uglify())))
     .pipe(when(argv.prod, size({
       showFiles: true
     })))
